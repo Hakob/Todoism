e138169a852e6029d256ff86a2faaedb7271c86b
Hakob Arakelyan <hakob@cognaize.com>
2019-04-28 20:03:30 +0400

    Fixd Static files paths

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "resources", "dist", "static"),
-]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "resources", "dist", "static")
